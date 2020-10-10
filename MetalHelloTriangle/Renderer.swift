128e531aaae5669a5aa92973fbb0b49d845f2702
Jim Eckerlein <jim.eckerlein@gmail.com>
2020-10-10 12:04:56 +0200

    cleanup

--- MetalHelloTriangle/Renderer.swift
+++ MetalHelloTriangle/Renderer.swift
@@ -6,11 +6,11 @@ class Renderer: NSObject, MTKViewDelegate {
 
     public let device: MTLDevice
     let commandQueue: MTLCommandQueue
-    var dynamicUniformBuffer: MTLBuffer
     var pipelineState: MTLRenderPipelineState
     var depthState: MTLDepthStencilState
+    var uniformBuffer: MTLBuffer
     
-    var mtlVertexDescriptor = MTLVertexDescriptor()
+    var vertexDescriptor = MTLVertexDescriptor()
     var indexBuffer: MTLBuffer
     var positionBuffer: MTLBuffer
     var colorBuffer: MTLBuffer
@@ -19,45 +19,43 @@ class Renderer: NSObject, MTKViewDelegate {
 
     init?(metalKitView: MTKView) {
         self.device = metalKitView.device!
-        self.commandQueue = self.device.makeCommandQueue()!
-
-        self.dynamicUniformBuffer = self.device.makeBuffer(length: MemoryLayout<Uniforms>.stride,
-                                                           options: [MTLResourceOptions.storageModeShared])!
-        self.dynamicUniformBuffer.label = "UniformBuffer"
-
-        uniforms = UnsafeMutableRawPointer(dynamicUniformBuffer.contents()).bindMemory(to:Uniforms.self, capacity: 1)
-
+        self.commandQueue = device.makeCommandQueue()!
+        
         metalKitView.depthStencilPixelFormat = MTLPixelFormat.depth32Float_stencil8
         metalKitView.colorPixelFormat = MTLPixelFormat.bgra8Unorm_srgb
         metalKitView.sampleCount = 1
-        
-        mtlVertexDescriptor.attributes[VertexAttribute.position.rawValue].format = MTLVertexFormat.float3
-        mtlVertexDescriptor.attributes[VertexAttribute.position.rawValue].offset = 0
-        mtlVertexDescriptor.attributes[VertexAttribute.position.rawValue].bufferIndex = BufferIndex.meshPositions.rawValue
-        
-        mtlVertexDescriptor.attributes[VertexAttribute.color.rawValue].format = MTLVertexFormat.float3
-        mtlVertexDescriptor.attributes[VertexAttribute.color.rawValue].offset = 0
-        mtlVertexDescriptor.attributes[VertexAttribute.color.rawValue].bufferIndex = BufferIndex.meshColors.rawValue
-        
-        mtlVertexDescriptor.layouts[BufferIndex.meshPositions.rawValue].stride = MemoryLayout<SIMD3<Float>>.stride
-        mtlVertexDescriptor.layouts[BufferIndex.meshPositions.rawValue].stepRate = 1
-        mtlVertexDescriptor.layouts[BufferIndex.meshPositions.rawValue].stepFunction = MTLVertexStepFunction.perVertex
-        
-        mtlVertexDescriptor.layouts[BufferIndex.meshColors.rawValue].stride = MemoryLayout<SIMD3<Float>>.stride
-        mtlVertexDescriptor.layouts[BufferIndex.meshColors.rawValue].stepRate = 1
-        mtlVertexDescriptor.layouts[BufferIndex.meshColors.rawValue].stepFunction = MTLVertexStepFunction.perVertex
 
-        let library = device.makeDefaultLibrary()
+        self.uniformBuffer = device.makeBuffer(length: MemoryLayout<Uniforms>.stride, options: [MTLResourceOptions.storageModeShared])!
+        self.uniformBuffer.label = "UniformBuffer"
+        uniforms = UnsafeMutableRawPointer(uniformBuffer.contents()).bindMemory(to: Uniforms.self, capacity: 1)
         
-        let vertexFunction = library?.makeFunction(name: "vertexShader")
-        let fragmentFunction = library?.makeFunction(name: "fragmentShader")
+        vertexDescriptor.attributes[VertexAttribute.position.rawValue].format = MTLVertexFormat.float3
+        vertexDescriptor.attributes[VertexAttribute.position.rawValue].offset = 0
+        vertexDescriptor.attributes[VertexAttribute.position.rawValue].bufferIndex = BufferIndex.meshPositions.rawValue
+        
+        vertexDescriptor.attributes[VertexAttribute.color.rawValue].format = MTLVertexFormat.float3
+        vertexDescriptor.attributes[VertexAttribute.color.rawValue].offset = 0
+        vertexDescriptor.attributes[VertexAttribute.color.rawValue].bufferIndex = BufferIndex.meshColors.rawValue
+        
+        vertexDescriptor.layouts[BufferIndex.meshPositions.rawValue].stride = MemoryLayout<SIMD3<Float>>.stride
+        vertexDescriptor.layouts[BufferIndex.meshPositions.rawValue].stepRate = 1
+        vertexDescriptor.layouts[BufferIndex.meshPositions.rawValue].stepFunction = MTLVertexStepFunction.perVertex
+        
+        vertexDescriptor.layouts[BufferIndex.meshColors.rawValue].stride = MemoryLayout<SIMD3<Float>>.stride
+        vertexDescriptor.layouts[BufferIndex.meshColors.rawValue].stepRate = 1
+        vertexDescriptor.layouts[BufferIndex.meshColors.rawValue].stepFunction = MTLVertexStepFunction.perVertex
+
+        let library = device.makeDefaultLibrary()!
+        
+        let vertexFunction = library.makeFunction(name: "vertexShader")
+        let fragmentFunction = library.makeFunction(name: "fragmentShader")
         
         let pipelineDescriptor = MTLRenderPipelineDescriptor()
         pipelineDescriptor.label = "RenderPipeline"
         pipelineDescriptor.sampleCount = metalKitView.sampleCount
         pipelineDescriptor.vertexFunction = vertexFunction
         pipelineDescriptor.fragmentFunction = fragmentFunction
-        pipelineDescriptor.vertexDescriptor = mtlVertexDescriptor
+        pipelineDescriptor.vertexDescriptor = vertexDescriptor
         
         pipelineDescriptor.colorAttachments[0].pixelFormat = metalKitView.colorPixelFormat
         pipelineDescriptor.depthAttachmentPixelFormat = metalKitView.depthStencilPixelFormat
@@ -73,23 +71,15 @@ class Renderer: NSObject, MTKViewDelegate {
         let depthStateDescriptor = MTLDepthStencilDescriptor()
         depthStateDescriptor.depthCompareFunction = MTLCompareFunction.less
         depthStateDescriptor.isDepthWriteEnabled = true
-        self.depthState = device.makeDepthStencilState(descriptor:depthStateDescriptor)!
+        depthState = device.makeDepthStencilState(descriptor:depthStateDescriptor)!
         
         let indices: [UInt16] = [0, 1, 2]
         let positions: [SIMD3<Float>] = [.init(-1, -1, 0), .init(1, -1, 0), .init(0, 1, 0)];
         let colors: [SIMD3<Float>] = [.init(1, 0, 0), .init(0, 1, 0), .init(0, 0, 1)];
         
-        indexBuffer = device.makeBuffer(bytes: indices,
-                                        length: MemoryLayout<UInt16>.stride * indices.count,
-                                        options: .cpuCacheModeWriteCombined)!
-        
-        positionBuffer = device.makeBuffer(bytes: positions,
-                                           length: MemoryLayout<SIMD3<Float>>.stride * positions.count,
-                                           options: .cpuCacheModeWriteCombined)!
-        
-        colorBuffer = device.makeBuffer(bytes: colors,
-                                        length: MemoryLayout<SIMD3<Float>>.stride * colors.count,
-                                        options: .cpuCacheModeWriteCombined)!
+        indexBuffer = device.makeBuffer(bytes: indices, length: MemoryLayout<UInt16>.stride * indices.count, options: .cpuCacheModeWriteCombined)!
+        positionBuffer = device.makeBuffer(bytes: positions, length: MemoryLayout<SIMD3<Float>>.stride * positions.count, options: .cpuCacheModeWriteCombined)!
+        colorBuffer = device.makeBuffer(bytes: colors, length: MemoryLayout<SIMD3<Float>>.stride * colors.count, options: .cpuCacheModeWriteCombined)!
 
         super.init()
     }
@@ -113,8 +103,8 @@ class Renderer: NSObject, MTKViewDelegate {
         renderEncoder.setRenderPipelineState(pipelineState)
         renderEncoder.setDepthStencilState(depthState)
         
-        renderEncoder.setVertexBuffer(dynamicUniformBuffer, offset: 0, index: BufferIndex.uniforms.rawValue)
-        renderEncoder.setFragmentBuffer(dynamicUniformBuffer, offset: 0, index: BufferIndex.uniforms.rawValue)
+        renderEncoder.setVertexBuffer(uniformBuffer, offset: 0, index: BufferIndex.uniforms.rawValue)
+        renderEncoder.setFragmentBuffer(uniformBuffer, offset: 0, index: BufferIndex.uniforms.rawValue)
         renderEncoder.setVertexBuffer(positionBuffer, offset: 0, index: BufferIndex.meshPositions.rawValue)
         renderEncoder.setVertexBuffer(colorBuffer, offset: 0, index: BufferIndex.meshColors.rawValue)
         
@@ -129,7 +119,5 @@ class Renderer: NSObject, MTKViewDelegate {
     }
 
     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
-        /// Respond to drawable size or orientation changes here
-        //aspect = Float(size.width) / Float(size.height)
     }
 }
